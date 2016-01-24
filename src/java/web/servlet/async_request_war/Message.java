e19b3c6f9978010f3fbed8b13081309791cf4934
adewaleafolabi <adewaleafolabi@gmail.com>
2016-01-24 22:21:59 +0100

    -Added Message class

---
+++ src/java/web/servlet/async_request_war/Message.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.servlet.async_request_war;
+
+/**
+ *
+ * @author vanso
+ */
+public class Message {
+    
+}
